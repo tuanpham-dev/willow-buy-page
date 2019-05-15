6c0a6d3c3019e2f800532531e96d0fccbe5a01a8
Tuan Pham <joomcoder@gmail.com>
2019-05-15 12:46:09 -0400

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-
+var rename = require('gulp-rename');
+var replace = require('gulp-replace');
 
 gulp.task('sass', function () {
   return gulp.src('./app/sass/**/*.scss')
@@ -13,3 +14,18 @@ gulp.task('sass:watch', gulp.series('sass', function () {
 }));
 
 gulp.task('default', gulp.parallel('sass:watch'));
+
+gulp.task('build', function() {
+  gulp.src(['./app/assets/**/*.png', './app/assets/**/*.svg'])
+    .pipe(gulp.dest('./dist/shared/images'));
+
+  gulp.src(['./app/liquid/**/*.liquid'])
+    .pipe(replace('asset_img_url2', 'asset_img_url'))
+    .pipe(gulp.dest('./dist/shopify/dist'));
+
+  return gulp.src('./app/sass/style.scss')
+    .pipe(replace(/url\(([^)]*)\)/, "url(#{'{{ $1 | asset_url }}'})"))
+    .pipe(rename({ basename: 'buy-page-2.0'}))
+    .pipe(gulp.dest('./dist/shared/css/reskin'));
+});
+
