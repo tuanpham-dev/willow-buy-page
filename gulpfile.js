1e8969246ba96ff87ab55eb80431b4dc5810b65d
Tuan Pham <joomcoder@gmail.com>
2019-05-16 22:43:27 -0400

    Change app folder to docs for github pages.

--- gulpfile.js
+++ gulpfile.js
@@ -4,27 +4,27 @@ var rename = require('gulp-rename');
 var replace = require('gulp-replace');
 
 gulp.task('sass', function () {
-  return gulp.src('./app/sass/**/*.scss')
+  return gulp.src('./docs/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./app/assets'));
+    .pipe(gulp.dest('./docs/assets'));
 });
 
 gulp.task('sass:watch', gulp.series('sass', function () {
-  gulp.watch('./app/sass/**/*.scss', gulp.series('sass'));
+  gulp.watch('./docs/sass/**/*.scss', gulp.series('sass'));
 }));
 
 gulp.task('default', gulp.parallel('sass:watch'));
 
 gulp.task('build', function() {
-  gulp.src(['./app/assets/**/*.png', './app/assets/**/*.svg'])
+  gulp.src(['./docs/assets/**/*.png', './docs/assets/**/*.svg'])
     .pipe(gulp.dest('./dist/shared/images'));
 
-  gulp.src(['./app/liquid/**/*.liquid'])
+  gulp.src(['./docs/liquid/**/*.liquid'])
     .pipe(replace('asset_img_url_local', 'asset_img_url'))
     .pipe(replace('asset_url_local', 'asset_img_url'))
     .pipe(gulp.dest('./dist/shopify/dist'));
 
-  return gulp.src('./app/sass/style.scss')
+  return gulp.src('./docs/sass/style.scss')
     .pipe(rename({ basename: 'buy-page-2.0'}))
     .pipe(replace(/url\(([^\)]+)\)/g, "url(#{'{{ $1 | asset_url }}'})"))
     .pipe(gulp.dest('./dist/shared/css/reskin'));
